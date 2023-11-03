743c7dc4c6d1c4d477719f26daf27fe62cb948e5
Brandon Liu <bdon@bdon.org>
2023-11-02 19:06:46 -0700

    add show and create entry points

---
+++ app/src/Create.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CreateComponent from './CreateComponent.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <CreateComponent />
+  </React.StrictMode>,
+)
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from "path";
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        create: resolve(__dirname, "index.html"),
+        show: resolve(__dirname, "show/index.html"),
+      },
+    },
+  },
 })
