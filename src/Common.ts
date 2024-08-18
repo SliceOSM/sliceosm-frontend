13b5783cacf3b2a85842430dc30e246c08482c4f
Brandon Liu <bdon@bdon.org>
2024-08-18 13:41:18 +0900

    implement show page

--- src/Common.ts
+++ src/Common.ts
@@ -1,7 +1,8 @@
-export const API_ENDPOINT = (import.meta.env.VITE_API_ENDPOINT || "/api");
-export const FILES_ENDPOINT = (import.meta.env.VITE_FILES_ENDPOINT || "/files");
+export const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT || "/api";
+export const FILES_ENDPOINT = import.meta.env.VITE_FILES_ENDPOINT || "/files";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
+import { Polygon, MultiPolygon } from "geojson";
 
 export const initializeMap = (mapContainerRef: HTMLDivElement) => {
   if (maplibregl.getRTLTextPluginStatus() === "unavailable") {
@@ -23,3 +24,39 @@ export const initializeMap = (mapContainerRef: HTMLDivElement) => {
   map.addControl(new maplibregl.NavigationControl(), "top-right");
   return map;
 };
+
+export function getBounds(
+  geojson: Polygon | MultiPolygon,
+): [[number, number], [number, number]] {
+  let minX = Infinity,
+    minY = Infinity,
+    maxX = -Infinity,
+    maxY = -Infinity;
+
+  function updateBounds(coords: number[][]) {
+    for (const coord of coords) {
+      const [x, y] = coord;
+      if (x < minX) minX = x;
+      if (y < minY) minY = y;
+      if (x > maxX) maxX = x;
+      if (y > maxY) maxY = y;
+    }
+  }
+
+  if (geojson.type === "Polygon") {
+    for (const ring of geojson.coordinates) {
+      updateBounds(ring);
+    }
+  } else if (geojson.type === "MultiPolygon") {
+    for (const polygon of geojson.coordinates) {
+      for (const ring of polygon) {
+        updateBounds(ring);
+      }
+    }
+  }
+
+  return [
+    [minX, minY],
+    [maxX, maxY],
+  ];
+}
--- src/ShowComponent.tsx
+++ src/ShowComponent.tsx
@@ -1,22 +1,67 @@
 import { useState, useEffect, useRef } from "react";
-import { API_ENDPOINT, FILES_ENDPOINT, initializeMap } from "./Common";
+import {
+  API_ENDPOINT,
+  FILES_ENDPOINT,
+  initializeMap,
+  getBounds,
+} from "./Common";
 import { Header } from "./CommonComponents";
 import maplibregl from "maplibre-gl";
+
+const getUuid = () => {
+  const query = new URLSearchParams(location.search);
+  return query.get("uuid");
+};
+
+interface Result {
+  Uuid: string;
+  Timestamp: string;
+  ElemsTotal: number;
+  Complete: boolean;
+  SizeBytes: number;
+  Elapsed: number;
+}
 
 function ShowComponent() {
   const mapContainerRef = useRef<HTMLDivElement>(null);
   const mapRef = useRef<maplibregl.Map>();
-  const [name, setName] = useState<string>();
+  const [result, setResult] = useState<Result>();
 
   const download = () => {
-    const query = new URLSearchParams(location.search);
-    const uuid = query.get("uuid");
-    window.location.href = `${FILES_ENDPOINT}/${uuid}.osm.pbf`;
+    window.location.href = `${FILES_ENDPOINT}/${getUuid()}.osm.pbf`;
   };
 
   useEffect(() => {
     const map = initializeMap(mapContainerRef.current!);
     mapRef.current = map;
+    fetch(`${FILES_ENDPOINT}/${getUuid()}_region.json`)
+      .then((resp) => resp.json())
+      .then((j) => {
+        map.on("load", () => {
+          try {
+            map.addSource("region", {
+              type: "geojson",
+              data: j.SanitizedRegionData,
+            });
+            map.addLayer({
+              id: "region",
+              type: "fill",
+              source: "region",
+              paint: {
+                "fill-color": "steelblue",
+                "fill-opacity": 0.5,
+              },
+            });
+
+            map.fitBounds(getBounds(j.SanitizedRegionData), {
+              padding: 60,
+              animate: false,
+            });
+          } catch (e) {
+            console.error(e);
+          }
+        });
+      });
   });
 
   useEffect(() => {
@@ -25,38 +70,39 @@ function ShowComponent() {
     fetch(`${API_ENDPOINT}/${uuid}`)
       .then((x) => x.json())
       .then((j) => {
-        console.log(j);
-        setName(j.Name);
-        if (j.SanitizedRegionType === "geojson") {
-          const geojson = j.SanitizedRegionData;
-          const poly_coords = geojson.coordinates[0]
-            .slice(0, -1)
-            .map((x:[number, number]) => [x[1], x[0]]);
-          console.log(geojson);
-          console.log(poly_coords);
-        } else if (j.SanitizedRegionType === "bbox") {
-          const split = j.SanitizedRegionData;
-          console.log(split);
+        if (j.Complete) {
+          setResult(j);
         }
+        // if (j.SanitizedRegionType === "geojson") {
+        //   const geojson = j.SanitizedRegionData;
+        //   const poly_coords = geojson.coordina;tes[0]
+        //     .slice(0, -1)
+        //     .map((x: [number, number]) => [x[1], x[0]]);
+        // } else if (j.SanitizedRegionType === "bbox") {
+        //   const split = j.SanitizedRegionData;
+        // }
       });
 
-    return () => {
-    };
+    return () => {};
   }, []);
 
   return (
     <div className="main">
-      <Header/>
+      <Header />
       <div className="content">
         <div className="sidebar">
-          { name }
+          {result ? (
+            <div>
+              <p>Snapshot Time {result.Timestamp}</p>
+              <p>Elements {result.ElemsTotal}</p>
+              <p>Size {result.SizeBytes}</p>
+              <p>Time Elapsed {result.Elapsed}</p>
+            </div>
+          ) : null}
           <button onClick={download}>Download</button>
         </div>
         <div className="mapContainer">
-          <div
-            ref={mapContainerRef}
-            className="map"
-          ></div>
+          <div ref={mapContainerRef} className="map"></div>
         </div>
       </div>
     </div>
